25e6973abd5537e7416bbe6fa9ff22aaabf8c612
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-15 17:20:45 +0100

    implement logout

--- mycarvalue/src/users/users.controller.ts
+++ mycarvalue/src/users/users.controller.ts
@@ -50,6 +50,11 @@ export class UsersController {
     return user;
   }
 
+  @Post('signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
+
   @Get(':id')
   async find(@Param('id') id: string) {
     const user = await this.usersService.findOne(+id);
