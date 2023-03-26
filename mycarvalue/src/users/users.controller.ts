f31fc32b8f70fe904555eb81c9569c63e7af2861
Luka Ernestini <luka.ernestini@gmail.com>
2023-03-26 11:54:18 +0200

    switch to global scoped interceptor

--- mycarvalue/src/users/users.controller.ts
+++ mycarvalue/src/users/users.controller.ts
@@ -9,7 +9,6 @@ import {
   Post,
   Query,
   Session,
-  UseInterceptors,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -18,12 +17,10 @@ import { UsersService } from './users.service';
 import { UserDto } from './dtos/user.dto';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
 
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
 export class UsersController {
   constructor(
     private usersService: UsersService,
