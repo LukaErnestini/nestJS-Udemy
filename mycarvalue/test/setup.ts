994bcb2b7c9d4b944f2617f2e079192441c2ceab
Luka Ernestini <luka.ernestini@gmail.com>
2023-04-02 11:31:46 +0200

    add todo comment

---
+++ mycarvalue/test/setup.ts
@@ -0,0 +1,4 @@
+// TODO: For a global "afterEach" that would clean up the database.
+// In the tutorial he deletes the db file, but I'm using Postgres
+// and didn't know how to make it work.
+// Maybe he will show it in the following videos.
